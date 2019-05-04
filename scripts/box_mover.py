0fc8985b7aabe80af3a71bc8cb023f0ba9eae343
Laura Bielenberg <laura.bielenberg@gmx.de>
2019-05-04 23:09:06 +0200

    included mover into the_claw

--- scripts/box_mover.py
+++ scripts/box_mover.py
@@ -9,12 +9,23 @@ from roboy_middleware_msgs.msg import MotorCommand
 class Mover():
     "Class that calculates pwm inputs depending on velocities input (RPM)."
 
-    def __init__(self, servo_pwm_pins, velocities):
-        self.servo_pwm_pins = servo_pwm_pins
-        self.velocities = velocities
-        self.set_pwm_freq = []
+    def __init__(self, servo_pwm_pin, velocity):
+        self.max_vel = 130 # max RPM
+        self.min_vel = -130
+        self.servo_pwm_pin = servo_pwm_pin
+        self.velocity = velocity
+        if not (velocity <= 130 and velocity >= -130):
+            print("Out of range! Valid velocity range is -130 to 130.")
 
     def map_velocities_to_pwm_signal(self):
-        stepsize = 0.3077
-        clockwise_signal = [270, 310]
-        counter_clockwise_signal = [325, 365]
+        stepsize = 0.3076 # 40/130
+        max_clockwise_signal = [270] # min_clockwise_signal = 310 - fastest at min
+        min_counter_clockwise_signal = [325] # max_counter_clockwise_signal = 365 - fastest at max
+        pwm_signal = []
+        if (self.velocity < 0 and self.velocity >= -130):
+            pwm_signal = max_clockwise_signal - self.velocity*stepsize
+        elif (self.velocity > 0 and self.velocity <= 130) :
+            pwm_signal = min_counter_clockwise_signal + self.velocity*stepsize
+        pwm_signal = 0
+
+        return pwm_signal
--- scripts/the_claw.py
+++ scripts/the_claw.py
@@ -7,28 +7,23 @@ import Adafruit_PCA9685
 import rospy
 from roboy_middleware_msgs.msg import MotorCommand
 from std_msgs.msg import Int8
+import box_mover as Mover
 
 def motor_command_callback(msg):
     for (motor,setpoint) in zip(msg.motors,msg.set_points):
-    	set_servo_pulse(motor,int(setpoint))
-	#rospy.loginfo(str(motor) + " " + str(setpoint))
+        set_servo_pulse(motor,int(setpoint))
+        #rospy.loginfo(str(motor) + " " + str(setpoint))
 
 # Helper function to make setting a servo pulse width simpler.
-def set_servo_pulse(channel, pulse):
-    pulse_length = 1000000    # 1,000,000 us per second
-    pulse_length //= 50       # 50 Hz
-    print('{0}us per period'.format(pulse_length))
-    pulse_length //= 4096     # 12 bits of resolution
-    print('{0}us per bit'.format(pulse_length))
-    pulse1 = pulse*1000
-    pulse2 = pulse1//pulse_length
-    print('{0}us pulse'.format(pulse2))
-    pwm.set_pwm(channel,0, pulse2)
-
+def set_servo_pulse(channel, input_speed):
+    mover = Mover(channel, input_speed)
+    output_signal = mover.map_velocities_to_pwm_signal()
+    pwm.set_pwm_freq(50)
+    pwm.set_pwm(channel, 0, output_signal)
 
 
 def set_speed_callback(msg):
-    set_servo_pulse(7, msg.data)
+    pwm.set_all_pwm(0, msg.data)
    # pwm.set_pwm(7, 0,msg.data)
    # time.sleep(5)
    # pwm.set_pwm(7, 0, 0)
@@ -37,15 +32,19 @@ def set_speed_callback(msg):
 if __name__ == '__main__':
     # Initialise the PCA9685 using the default address (0x40).
     pwm = Adafruit_PCA9685.PCA9685()
-    # Set frequency to 60hz, good for servos.
+   # Set frequency to 60hz, good for servos.
     print('Starting \n')
     pwm.set_pwm_freq(50)
-    pwm.set_pwm(7,0, 200)
-    print('Sleeping \n ')
+    pwm.set_pwm(7,0,150)
+    print('going at 150 = min \n ')
     time.sleep(3)
-    pwm.set_pwm(7,0,0)
+    pwm.set_pwm(7,0,300)
+    print('going at 300 \n ')
+    time.sleep(3)
+    pwm.set_pwm(7,0,350)
+    print('going at 350 \n ')
     print('Finished')
-    # Initialize the node and name it.
+  # Initialize the node and name it.
     rospy.init_node('the_claw')
     rospy.Subscriber("the_claw/MotorCommand", MotorCommand, motor_command_callback)
     rospy.Subscriber("the_claw/SetSpeed", Int8, set_speed_callback)
